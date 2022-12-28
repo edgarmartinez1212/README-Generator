f0273812693db5f81f923cf68f799e2dfc539993
Edgar Martinez <edgarmartinez@Edgars-MBP.attlocal.net>
2022-12-27 20:41:50 -0600

    restructured folders- accidently deleted index.js- luckily had it saved on repo- edited index.js

---
+++ utils/README.md
@@ -0,0 +1,57 @@
+# test
+
+## Description
+test
+
+---
+
+## Table of Contents
+- ### [Installation](#installation)
+
+- ### [Usage](#usuage)
+
+- ### [Contributing](#contributing)
+
+- ### [Tests](#tests)
+
+- ### [Questions](#questions)
+
+- ### [License](#license)
+
+---
+
+## Installation
+test
+
+---
+
+## Usuage
+test
+
+---
+
+## Contributing
+test
+
+---
+
+## Tests
+test
+
+---
+
+## Questions
+If you have any questions, please visit my [GitHub](https://github.com/tet) or [Email](test) me.
+
+GitHub: https://github.com/tet
+
+Email: test
+
+---
+
+## License
+  This project is licensed under MIT
+
+
+<img src="https://img.shields.io/badge/license-MIT-blue.svg">
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,53 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return `this will be a badge`;
+  if (license === "None") return "";
+  return `<img src="https://img.shields.io/badge/license-${license}-blue.svg">\n`;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return ``;
+  if (license === "None") return "";
+  return `- ### [License](#license)`;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return ``;
+  if (license === "None") return "";
+  return `## License
+  This project is licensed under ${license}\n`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-## ${data.description}
+## Description
+${data.description}
+
+---
 
 ## Table of Contents
-- [Installation](#installation)
-- [Usage](#usuage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
+- ### [Installation](#installation)\n
+- ### [Usage](#usuage)\n
+- ### [Contributing](#contributing)\n
+- ### [Tests](#tests)\n
+- ### [Questions](#questions)\n
+${renderLicenseLink(data.license)}
+
+---
 
 ## Installation
 ${data.installation}
 
-## Usuage
-${data.usuage}
+---
 
-## License
-${renderLicenseSection(data.license)}
+## Usuage
+${data.usage}
+
+---
 
 ## Contributing
 ${data.contributing}
 
+---
+
 ## Tests
 ${data.tests}
 
+---
+
 ## Questions
+If you have any questions, please visit my [GitHub](https://github.com/${data.github}) or [Email](${data.email}) me.\n
 GitHub: https://github.com/${data.github}\n
 Email: ${data.email}
+
+---
+
+${renderLicenseSection(data.license)}\n
+${renderLicenseBadge(data.license)}
 `;
 }
 
